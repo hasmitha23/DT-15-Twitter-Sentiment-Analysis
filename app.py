e4e87d326a0348f41f4b6d85e7c53f75c4f7b038
Rithvika Mylavarapu <rithvika451@gmail.com>
2025-02-12 20:26:40 +0530

    day 6

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,jsonify
+from test import TextToNum
+import pickle
 app=Flask(__name__)
 @app.route("/")
 def home():
@@ -8,7 +10,22 @@ def predict():
     if request.method=="POST":
         msg=request.form.get("message")
         print(msg)
+        ob=TextToNum(msg)
+        ob.cleaner()
+        ob.token()
+        ob.removeStop()
+        st=ob.stemme()
+        with open('vectorizer.pickle', 'rb') as vcfile:
+            vc = pickle.load(vcfile)
+        stvc=" ".join(st)
+        data=vc.transform([stvc])
+        print(data)
+        with open('model.pickle', 'rb') as mdfile:
+            model = pickle.load(mdfile)
+        pred=model.predict(data)
+        return jsonify({"result":str(pred[0])})
+
     else:
         return render_template("predict.html")
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5050)
+    app.run(host="0.0.0.0",port=5000)
